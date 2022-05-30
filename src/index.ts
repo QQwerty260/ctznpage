a99fb867680ce0d5622e8f6fda439b8d8f523dc8
Your Name <you@example.com>
2022-05-30 14:55:01 +0000

    fix 3

--- src/index.ts
+++ src/index.ts
@@ -125,8 +125,9 @@ ${
   }
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("/-", (req, res) => {
+  console.log(`__dirname: `, __dirname)
   const md = new MobileDetect(req.headers["user-agent"] as string);
   const isBot = md.is("Bot");
   if (isBot) {
@@ -134,7 +135,7 @@ app.get("/-", (req, res) => {
     return;
   }
 
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 app.listen(port, () => {
